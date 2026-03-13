ace0bd02de44c04b01db3e0ab979116a2b2a502e
YEOP <cxv963@gmail.com>
2026-03-14 02:56:22 +0900

    perf: WebGL/3D API 비활성화로 SwiftShader GPU 부하 추가 감소

--- src/browser.py
+++ src/browser.py
@@ -92,6 +92,9 @@ class BrowserManager:
                 # SwiftShader CPU 부하 감소 (헤드리스 Linux 렌더링 최적화)
                 # 주의: gpu-compositing/threaded-compositing 비활성화는 마우스 휠 데드락 유발 → 제외
                 "--disable-gpu",                       # GPU 프로세스(SwiftShader) 자체 비활성화
+                "--disable-webgl",                     # WebGL 비활성화 (SwiftShader 작업 감소)
+                "--disable-webgl2",                    # WebGL2 비활성화
+                "--disable-3d-apis",                   # 3D API 비활성화
                 "--disable-accelerated-video-decode",  # HW 비디오 디코딩 비활성화
                 "--disable-accelerated-video-encode",  # HW 비디오 인코딩 비활성화
                 "--num-raster-threads=1",              # 래스터화 스레드 1개로 제한
